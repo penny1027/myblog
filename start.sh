9d5b8f0488e355816c88e9c409999ca7a64a5c43
penny <1058449090@qq.com>
2025-09-28 12:47:00 +0800

    commit 1

--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-#chmod +x /usr/local/bin/docker-compose
